8b825b377e95313ae2e4a686fef3d067054f8f53
oCaioAug <ocaioaug.dev@protonmail.com>
2025-03-19 00:55:08 -0300

    Coloca controlle Produto como página principal

--- GestaoDeEstoque/Program.cs
+++ GestaoDeEstoque/Program.cs
@@ -26,7 +26,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Produto}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
