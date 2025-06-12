0051fa8c3a64f30632710932d4b4bb7eb4dea7eb
oCaioAug <ocaioaug.dev@protonmail.com>
2025-06-12 08:21:04 -0300

    feat: add migration class for Terceira with model definitions for Fornecedor, Produto, and TipoProduto

---
+++ GestaoDeEstoque/Migrations/20250609220916_Terceira.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestaoDeEstoque.Migrations
+{
+    /// <inheritdoc />
+    public partial class Terceira : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
